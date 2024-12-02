644ed39da9c4ae92296883c8d4ff12e5ee9ab78f
Yug <yugsuhagiya2005@gmail.com>
2024-12-02 17:39:26 +0530

    add fun of age

--- index.c
+++ index.c
@@ -2,10 +2,14 @@
 int main(){
     printf("jay swaminarayan");
     printf("hello");
-    int a=1;
+    int c=1;
     return 0;
 }
 
 void fun(){
     int b=2;
+}
+
+void age(){
+    prinf("as");
 }
